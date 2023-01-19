03063321ea9abe42768074b3ad193c54579896e9
samFabrice <dsamfabrice5@gmail.com>
2023-01-19 16:45:06 +0100

    Add UserController and user files

--- movieManagement/src/main/java/com/example/demo/controller/UserController.java
+++ movieManagement/src/main/java/com/example/demo/controller/UserController.java
@@ -11,5 +11,36 @@ public class UserController {
     public ResponseEntity<List<String>> getAllUsers() {
         return ResponseEntity.ok(new ArrayList<>());
     }
+    import org.springframework.web.bind.annotation.*;
+import org.springframework.http.*;
+
+import com.polytech.auth.model.User;
+
+import java.util.*;
+
+    @RequestMapping("/users")
+    @RestController
+    public class UserController {
+
+        @GetMapping("")
+        public ResponseEntity<List<User>> getAllUsers() {
+
+            User user1 = new User();
+            user1.setName("user1");
+            user1.setEmail("mail@test.com");
+
+            User user2 = new User();
+            user2.setName("user2");
+            user2.setEmail("mail2@test.com");
+
+            List<User> list = new ArrayList<>();
+            list.add(user1);
+            list.add(user2);
+
+            return ResponseEntity.ok(list);
+        }
+
+    }
+
 
 }
---
+++ movieManagement/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,9 @@
+import lombok.*;
+
+@Data
+public class User {
+
+    private String name;
+    private String email;
+
+}
