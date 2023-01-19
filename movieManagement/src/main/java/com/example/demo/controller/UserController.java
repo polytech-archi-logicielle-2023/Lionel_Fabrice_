d786f42c734869bbf93a065ce928a0d87a9be0d2
samFabrice <dsamfabrice5@gmail.com>
2023-01-19 17:10:42 +0100

    updated all

--- movieManagement/src/main/java/com/example/demo/controller/UserController.java
+++ movieManagement/src/main/java/com/example/demo/controller/UserController.java
@@ -1,5 +1,7 @@
 import org.springframework.web.bind.annotation.*;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+
+import com.example.demo.model.User;
 
 import java.util.*;
 
@@ -8,39 +10,22 @@ import java.util.*;
 public class UserController {
 
     @GetMapping("")
-    public ResponseEntity<List<String>> getAllUsers() {
-        return ResponseEntity.ok(new ArrayList<>());
+    public ResponseEntity<List<User>> getAllUsers() {
+
+        User user1 = new User();
+        user1.setName("user1");
+        user1.setEmail("mail@test.com");
+
+        User user2 = new User();
+        user2.setName("user2");
+        user2.setEmail("mail2@test.com");
+
+        List<User> list = new ArrayList<>();
+        list.add(user1);
+        list.add(user2);
+
+        return ResponseEntity.ok(list);
     }
-    import org.springframework.web.bind.annotation.*;
-import org.springframework.http.*;
-
-import com.polytech.auth.model.User;
-
-import java.util.*;
-
-    @RequestMapping("/users")
-    @RestController
-    public class UserController {
-
-        @GetMapping("")
-        public ResponseEntity<List<User>> getAllUsers() {
-
-            User user1 = new User();
-            user1.setName("user1");
-            user1.setEmail("mail@test.com");
-
-            User user2 = new User();
-            user2.setName("user2");
-            user2.setEmail("mail2@test.com");
-
-            List<User> list = new ArrayList<>();
-            list.add(user1);
-            list.add(user2);
-
-            return ResponseEntity.ok(list);
-        }
-
-    }
-
 
 }
+
